de144dc2a5bd3930f866ee69e0c4f708f86e02da
chandravellanki <vellanki.chandra@gmail.com>
2015-03-31 23:07:01 +0530

    Commit

--- WebLearning/Angular/Egghead.io/FirstPart/app.js
+++ WebLearning/Angular/Egghead.io/FirstPart/app.js
@@ -1,35 +1,1 @@
-//Wrapping the JS in a closure is a good habit.
-(function(){
-	var app = angular.module('store',[ ]);//This array contains the dependencies.
-	
-	//Controller. The controller should always be Caps
-	app.controller('StoreController', function(){
-		//Assign the objec to a property of controller.
-		this.products = gems;
-	});
-	
-	//our JS object that we want to display in HTML
-	var gems = [
-		{
-			name: 'Kohinoor',
-			price: 10.44,
-			description: 'The worlds costliest gem',
-			canPurchase: true,
-			soldOut: true
-		},
-		{
-			name: 'A Gem',
-			price: 10.44,
-			description: 'The worlds A gem',
-			canPurchase: true,
-			soldOut: false
-		},
-		{
-			name: 'B Gem',
-			price: 10.44,
-			description: 'The worlds B gem',
-			canPurchase: true,
-			soldOut: false
-		}
-	]
-})();
+//Wrapping the JS in a closure is a good habit.(function() {    var app = angular.module('store', []); //This array contains the dependencies.    //Controller. The controller should always be Caps    app.controller('StoreController', function() {        //Assign the objec to a property of controller.        this.products = gems;    });    app.controller('PanelController', function(){    		this.tab = 1;//We can refer to this using panel.tab    		this.selectedTab = function(setTab){    			this.tab = setTab;    		}    		this.isSelected = function(checkTab){    			return this.tab === checkTab;    		}    });    //our JS object that we want to display in HTML    var gems = [{        name: 'Kohinoor',        price: 10.44,        description: 'The worlds costliest gem',        canPurchase: false,        soldOut: true    }, {        name: 'A Gem',        price: 10.44,        description: 'The worlds A gem',        canPurchase: true,        soldOut: false    }, {        name: 'B Gem',        price: 10.44,        description: 'The worlds B gem',        canPurchase: true,        soldOut: false    }]})();
